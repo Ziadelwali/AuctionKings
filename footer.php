188d2e80ebd7c53ccec47df2d5a257af7daa2138
Imran A <imran786dk@gmail.com>
2015-02-27 09:58:35 +0100

    Revert "First Commit"
    This reverts commit 280b149231fe4cafc04508b39981477c2e922021.

--- footer.php
+++
@@ -1,7 +0,0 @@
-<br/><br/><br/>
-<!--Scripts-->
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="js/bootstrap.min.js"></script>
-</body>
-</html>
--- forum/forum_footer.php
+++
@@ -1,6 +0,0 @@
-<!--Scripts-->
-<script src="../js/jquery-1.11.1.min.js"></script>
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-</body>
-</html>
