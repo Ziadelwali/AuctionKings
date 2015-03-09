3e7cb4398ca3fab80fb775771523787e51ae8463
Casper Madsen <caverhtc@gmail.com>
2015-03-09 09:57:30 +0100

    auctions and auction w createbid site Signed-off-by: Casper Madsen <caverhtc@gmail.com>

--- auction.php
+++ auction.php
@@ -1,35 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
 <style>
 table, th, td {
-	border: 1px solid black;
+	border: 1px solid green;
+		text-align: center;
+		background-color: #9FF781;
+}
+#myTable table, th, td {
+	border: 1px solid green;
+		text-align: center;
+		background-color: #9FF781;
+}
+th {
+background-color: green;
+    color: white;
+}
+#myDiv
+{
+width:100%; 
+height:100%;
+margin:auto;
+display:block;
+}
+#myDiv img
+{
+max-width:100%; 
+max-height:100%;
+margin:auto;
+display:block;
 }
 </style>
+</head>
+<body>
 <?php
 	
 	/*
-		* @author     Casper Maro Madsen <iliketurtels@gmail.com>
+		* @author     Casper Maro Madsen <caverhtc@gmail.com>
 	*/
 	
 	include 'secure/db_connect.php';
 	include 'secure/functions.php';
+	sec_session_start(); // Our custom secure way of starting a php session.
 	
+	if(login_check($dbcon) == true AND $_SESSION['status'] == 0)
+	{
+	    include 'header.php';
+
+		include 'footer.php';
+	}
+		else if ($_SESSION['status'] == 1)
+			{
+				header("Location: index.php?logError=5");
+				// Destroy session
+				session_destroy();
+				exit;
+			}
+				else
+					{
+						echo 'You are not authorized to access this page, please login. <br/>';
+					}
+					
 	$auctionid = $_GET['id'];
-	$sql = "SELECT ID, Title, Picture, Higestbid, Timeend, Description, Bids FROM auctionking.view_all_auctions WHERE ID = $auctionid;";
+	$sql = "SELECT ID, Title, Picture, Higestbid, Timestart ,Timeend, Description, Username, Bids FROM auctionking.view_one_auction WHERE ID = $auctionid;";
 	$result = $dbcon->query($sql);
 
 	if ($result->num_rows > 0) {
-    echo "<table><tr><th>ID</th><th>Title</th><th>Picture</th><th>Higest Bid</th><th>Auction ends</th><th>Description</th><th>Bids</th></tr>";
+    echo "<table><tr><th>ID</th><th>Title</th><th>Picture</th><th>Higest Bid</th><th>Auction started</th><th>Auction ends</th><th>Description</th><th>Created by</th><th>Bids</th></tr>";
      // output data of each row
      while($row = $result->fetch_assoc()) {
 		 $id = $row["ID"];
 		 $title = $row["Title"];
 		 $pic = $row["Picture"];
 		 $higestbid = $row["Higestbid"];
+		 $timestart = $row["Timestart"];
 		 $timeleft = $row["Timeend"];
 		 $desc = $row["Description"];
+		 $user = $row["Username"];
 		 $bids = $row["Bids"];
 		 
-		echo '<tr><td>' . $id. '</td><td>' . $title. '</td><td><div id="myDiv"><a href=auction.php?id='.$id.'><img src="img/' . $pic. '"></a></div></td><td>' . $higestbid. '</td><td>' . $timeleft. '</td><td>' . $desc. '</td><td>' . $bids. "</td></tr>";
-		 
+		 $datetime1 = new DateTime();
+		 $datetime2 = new DateTime($timeleft);
+		 $d1 = $datetime1->format('Y-m-d H:i:s');
+		 $d2 = $datetime2->format('Y-m-d H:i:s');
+		 $timeFirst  = strtotime($d1);
+		 $timeSecond = strtotime($d2);
+		 $differenceInSeconds = $timeSecond - $timeFirst;
+		 	
+		 echo '<tr><td>' . $id. '</td><td>' . $title. '</td><td><div id="myDiv"><img src="img/' . $pic. '"></div></td><td>' . $higestbid. ' $' . '</td><td>' . $timestart. '</td><td><span id="timer">' . '</span></td><td width="400px">' . $desc. '</td><td>' . $user . '</td><td>' . $bids. "</td></tr>";
+		
         // echo '<tr><td>' . $row["ID"]. '</td><td>' . $row["Title"]. '</td><td><div id="myDiv"><a href="?id=$id"><img src="img/' . $row["Picture"]. '"></a></div></td><td>' . $row["Higestbid"]. '</td><td>' . $row["Timeend"]. '</td><td>' . $row["Description"]. '</td><td>' . $row["Bids"]. "</td></tr>";
 	 }
      echo "</table>";
@@ -37,45 +96,78 @@ table, th, td {
      echo "0 results";
 }
 
-$dbcon->close();
 
-function placebid()
-{
-	echo "1";
-	if(isset($_POST['bidinput']))
-	{
-		echo "2";
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$amount = $_POST['bidinput'];
-	
-	$query= "CALL create_bid (?,?,?)";
-		
-	if ($insert_stmt = $dbcon->prepare($query))
+	$user_id = $_SESSION['user_id'];
+	$call = $dbcon->prepare('CALL create_bid (?,?,?,@sqlstatus)');
+	$call->bind_param('iii', $amount, $user_id, $auctionid);
+	$call->execute();
+	$select = $dbcon->query('SELECT @sqlstatus');
+	$result = $select->fetch_assoc();
+	$sqlstatus     = $result['@sqlstatus'];
+	if ($sqlstatus == 0)
 	{
-		$insert_stmt->bind_param('iii', $amount, $user_id, $auctionid);
-	
-		// Execute the prepared query.
-		$insert_stmt->execute();
-		header("Location: ../?success=1");
-		// Make sure that code below does not get executed when we redirect.
-		exit;
+		echo "Bid Failed!";
 	}
-	else
+	if ($sqlstatus == 1)
 	{
-		header("Location: ../../../?regError=1");
-		// Make sure that code below does not get executed when we redirect.
-		exit;
+		echo "Bid Created!";
 	}
-  }
+	if ($sqlstatus == 2)
+	{
+		echo "Bid must be higher than last bid";
+	}
+	if ($sqlstatus == 3)
+	{
+		echo "You cannot bid on a ended auction";
+	}
+	if ($sqlstatus == 4)
+	{
+		echo "You cant bid on your own auctions";
+	}
 }
+
+$dbcon->close();
 
 ?>
 </br>
 </br>
-<table>
+<div id="myTable">
+<form method="post">
+		<table>
 	<tr>
 		<td>Enter your bid</td>
-		<td><input id="bidinputid" name="bidinput" type="number" min="0.1"></td>
+		<td><input id="bidinputid" name="bidinput" type="number"></td>
 		<td><input id="bidbtn" name="bidbtn" type="submit" value="Place Bid"
-			onclick="placebid()"></td>
+			onclick="placebidtemp()"></td>
 	</tr>
 </table>
+</form>	
+</div>
+</body>
+<script>
+	var countDownTime = "<?php echo $differenceInSeconds; ?>";
+	function countDownTimer() {
+		if (countDownTime < 0) {
+			var result = "Ended";
+			document.getElementById("timer").innerHTML = result;
+		}
+		else if (countDownTime > 0)
+		{
+			var days = parseInt( countDownTime / 86400);
+			var hours = parseInt( countDownTime / 3600 ) % 24;
+			var minutes = parseInt( countDownTime / 60 ) % 60;
+			var seconds = countDownTime % 60;
+			var result = (days < 10 ? "0" + days : days) + ":" + (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+			document.getElementById("timer").innerHTML = result;
+	   		if(countDownTime == 0  ){ countDownTime = 1 }
+	   		countDownTime = countDownTime - 1;
+	   		setTimeout(function(){ countDownTimer() }, 1000);
+		}
+			}
+			
+
+	countDownTimer();
+</script>
+</html>
--- auctions.php
+++ auctions.php
@@ -1,7 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+	border: 1px solid green;
+		text-align: center;
+		background-color: #9FF781;
+		width: 1000px
+}
+th {
+background-color: green;
+    color: white;
+}
+#myDiv img
+{
+max-width:100%; 
+max-height:100%;
+margin:auto;
+display:block;
+}
+</style>
+</head>
+<body>
 <?php
 	
 	/*
-		* @author     Ziad El-Wali <Ziadelwali@gmail.com>
+		* @author     Casper Maro Madsen <caverhtc@gmail.com>
 	*/
 	
 	include 'secure/db_connect.php';
@@ -11,115 +35,46 @@
 	if(login_check($dbcon) == true AND $_SESSION['status'] == 0)
 	{
 	    include 'header.php';
-	?>
+
+		include 'footer.php';
+	}
+		else if ($_SESSION['status'] == 1)
+			{
+				header("Location: index.php?logError=5");
+				// Destroy session
+				session_destroy();
+				exit;
+			}
+				else
+					{
+						echo 'You are not authorized to access this page, please login. <br/>';
+					}
 	
-	<div id="section">
-		<table style="width:100%">
-			<tr>
-				<td>
-					<h2>old vase</h2>
-					<a href="current_auction.php"><img src="img/vase3.jpg" alt="The picture could not be found" style="width:304px;height:228px"></a>
-					<p>
-						The vase's is very old. Hurry up to bit.
-					</p>
-				</td>
-				<td>
-					<h2>Nice vase</h2>
-					<img src="img/vase4.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-					<p>
-						Thoose two vase's belong's to each other. Therefore you can only bit at both of them.
-					</p>
-				</td>
-				<td>
-					<h2>ugly vase</h2>
-					<img src="img/vase2.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-					<p>
-						The vase's is very cheap. 
-					</p>
-				</td>
-				<td>
-					<h2>beatyfull vase</h2>
-					<img src="img/vase1.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-					<p>
-						The vase is a seldon old one and nearlyun broken.
-						<tr>
-							<td>
-								
-								
-								<h2>cheap vase</h2>
-								<a href="logIn.html"><img src="img/vase5.jpg" alt="The picture could not be found" style="width:304px;height:228px"></a>
-								<p>
-									The vase's is very cheap. Specially if you hurry up.
-								</p>
-							</td>
-							<td>
-								<h2>Nice vase</h2>
-								<img src="img/vase6.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-								<p>
-									The picture shows the vase
-								</p>
-							</td>
-							<td>
-								<h2>fine vase</h2>
-								<img src="img/vase7.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-								<p>
-									The vase's is to flowers.
-								</p>
-							</td>
-							<td>
-								<h2>Very nice vase</h2>
-								<img src="img/vase8.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-								<p>
-									No description on this vase. I mean, It's a vase.
-									<tr>
-										<td>
-											<h2>The vase</h2>
-											<a href="logIn.html"><img src="img/vase9.jpg" alt="The picture could not be found" style="width:304px;height:228px"></a>
-											<p>
-												Very beatyfull.
-											</p>
-										</td>
-										<td>
-											<h2>Nice vase</h2>
-											<img src="img/vase10.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-											<p>
-												Thoose two vase's belong's to each other. Therefore you can only bit at both of them.
-											</p>
-										</td>
-										<td>
-											<h2>ugly vase</h2>
-											<img src="img/vase12.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-											<p>
-												The vase's is very old. Hurry up to bit.
-											</p>
-										</td>
-										<td>
-											<h2>Nice vase</h2>
-											<img src="img/vase11.jpg" alt="The picture could not be found" style="width:304px;height:228px">
-											<p>
-												Thoose two vase's belong's to each other. Therefore you can only bit at both of them.
-											</p>
-										</td>
-									</table>
-									</div>
-									
-							
-							
-							
-							
-							
-							<?php
-								include 'footer.php';
-							}
-							else if ($_SESSION['status'] == 1)
-							{
-								header("Location: index.php?logError=5");
-								// Destroy session
-								session_destroy();
-								exit;
-							}
-							else
-							{
-								echo 'You are not authorized to access this page, please login. <br/>';
-							}
+	$sql = "SELECT ID, Title, Picture, Higestbid, Timeend, Description, Bids FROM auctionking.view_all_auctions";
+	$result = $dbcon->query($sql);
+
+	if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Title</th><th>Picture</th><th>Higest Bid</th><th>Auction ends</th><th>Description</th><th>Bids</th></tr>";
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+		 $id = $row["ID"];
+		 $title = $row["Title"];
+		 $pic = $row["Picture"];
+		 $higestbid = $row["Higestbid"];
+		 $timeleft = $row["Timeend"];
+		 $desc = $row["Description"];
+		 $bids = $row["Bids"];
+		 
+		echo '<tr><td>' . $id. '</td><td>' . $title. '</td><td><div id="myDiv"><a href=auction.php?id='.$id.'><img src="img/' . $pic. '"></a></div></td><td>' . $higestbid. ' $' . '</td><td>' . $timeleft. '</td><td>' . $desc. '</td><td>' . $bids. "</td></tr>";
+		 
+        // echo '<tr><td>' . $row["ID"]. '</td><td>' . $row["Title"]. '</td><td><div id="myDiv"><a href="?id=$id"><img src="img/' . $row["Picture"]. '"></a></div></td><td>' . $row["Higestbid"]. '</td><td>' . $row["Timeend"]. '</td><td>' . $row["Description"]. '</td><td>' . $row["Bids"]. "</td></tr>";
+	 }
+     echo "</table>";
+} else {
+     echo "0 results";
+}
+
+$dbcon->close();
 ?>
+</body>
+</html>
