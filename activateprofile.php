08ffdebd5fa354529cec2da412e230f11f8379f8
ZiadElwali <ZiadElwali@gmail.com>
2015-03-10 10:38:01 +0100

    Signed-off-by: ZiadElwali <ZiadElwali@gmail.com>

--- activateprofile.php
+++ activateprofile.php
@@ -5,7 +5,7 @@ include 'secure/functions.php';
 
 
 	//include_once 'analyticstracking.php';
-
+//
 sec_session_start(); // Our custom secure way of starting a php session.
 
 if(login_check($dbcon) == true)
